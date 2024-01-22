ddf19d0d76f90ceba71506e74b3afce516d8e5b3
Nadi Lysenko <nadirose@mail.ru>
2024-01-23 01:08:43 +0300

    Выполняет стилизацию бургер меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,6 +1,6 @@
 /* в этот файл добавляет скрипты*/
 // let navMain = document.querySelector('.main-nav');
-// let navToggle = document.querySelector('.header__toggle');
+// let navToggle = document.querySelector('.main-nav__toggle');
 
 // navMain.classList.remove('main-nav--nojs');
 
@@ -13,3 +13,18 @@
 //     navMain.classList.remove('main-nav--opened');
 //   }
 // });
+
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
